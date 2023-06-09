eb7812c109cbf9c914883954a54563b2b531f184
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-09 11:27:16 +0930

    Update steamlit_app1.py

--- steamlit_app1.py
+++ steamlit_app1.py
@@ -31,7 +31,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
