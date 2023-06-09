930d387495a975fc4b7c98919b350742906e9702
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-09 11:29:52 +0930

    Update steamlit_app1.py

--- steamlit_app1.py
+++ steamlit_app1.py
@@ -40,3 +40,5 @@ my_data_row = my_cur.fetchall()
 streamlit.text("Fruit list:")
 streamlit.dataframe(my_data_row)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+
