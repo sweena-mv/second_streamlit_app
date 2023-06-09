90f120a7dc657394e4ea388ce0356a417458ea01
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-09 12:27:35 +0930

    Update steamlit_app1.py

--- steamlit_app1.py
+++ steamlit_app1.py
@@ -31,13 +31,13 @@ try:
   #streamlit.write('The user entered ', fruit_choice)
     streamlit.error("Please select a fruit choice")
   else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  #streamlit.text(fruityvice_response.json())
-  # write your own comment -what does the next line do? 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-streamlit.error() 
+  streamlit.error() 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
