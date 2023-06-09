2d57c5f898a75c00fa5a52cc7cc921da64fb24d6
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-09 12:26:57 +0930

    Update steamlit_app1.py

--- steamlit_app1.py
+++ steamlit_app1.py
@@ -29,7 +29,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
   #streamlit.write('The user entered ', fruit_choice)
-  streamlit.error("Please select a fruit choice")
+    streamlit.error("Please select a fruit choice")
   else:
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
   #streamlit.text(fruityvice_response.json())
