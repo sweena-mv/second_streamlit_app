56dfec5a83e294203f3d7a933e4ed9a47856e20b
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-09 11:16:48 +0930

    Update steamlit_app1.py

--- steamlit_app1.py
+++ steamlit_app1.py
@@ -36,7 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("Fruit list:")
 streamlit.text(my_data_row)
 
